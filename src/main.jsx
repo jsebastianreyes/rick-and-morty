be50a8e56ab88774c110c4c2f9fa645c09e3132f
Sebastian Reyes <juansreyes.salinas@gmail.com>
2023-02-21 16:51:30 -0500

    ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import main from './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App /> 
